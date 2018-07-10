d91824c863a57071872cf146a8a47abcf4ff886a
Karol Perechowski <karol.perechowski@gmail.com>
2018-07-10 13:11:44 +0200

    reset in progress

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { TaskService } from '../shared/task.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -12,6 +12,19 @@ export class TaskComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
+  resetForm(form?: NgForm){
+    if(form!=null)
+      form.reset();
+    this.taskService.selectedTask = {
+      TaskID: null,
+      TaskName: "",
+      TaskDescription:"",
+      TaskDate: null,
+      TaskDificulty: 0
+
+    }
+  }
 }
